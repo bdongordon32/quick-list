11db692d53cf0a9ce51c0377d51c1748318b5f80
Brandon Gordon <bragor32@gmail.com>
2024-12-22 19:27:15 -0500

    -

--- lib/widgets/quick_list_card.dart
+++ lib/widgets/quick_list_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quick_list/app_theme.dart';
 import 'package:quick_list/models/quick_list.dart';
 
 class QuickListCard extends StatelessWidget {
@@ -9,8 +10,34 @@ class QuickListCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
-      child: Text(quickList.title),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(4)),
+        border: Border.all(
+          color: primaryDarkAccent,
+          width: 2
+        )
+      ),
+      padding: EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            quickList.title,
+            style: TextStyle(
+              fontSize: 20,
+              color: primaryDarkAccent,
+              fontWeight: FontWeight.w700
+            ),
+          ),
+          Text(
+            '3/8 items completed',
+            style: TextStyle(
+              color: primaryDarkAccent,
+              fontWeight: FontWeight.w500
+            ),
+          )
+        ]
+      ),
     );
   }
 }
