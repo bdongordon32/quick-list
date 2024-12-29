9072470a69c202ddf869447ddd2077db65a977cc
Brandon Gordon <bragor32@gmail.com>
2024-12-29 14:26:45 -0500

    =

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 final Color primaryDarkAccent = Color(0xFF5D6B85);
 final Color primaryLightAccent = Color(0xFFE8EEFF);
 
+final Color progressBarBase = Color.fromARGB(255, 183, 191, 206);
+final Color progressBarCompleted = Color(0xFF5D6B85);
+
 class QuickListTheme {
   ThemeData appTheme = ThemeData(
     useMaterial3: true,
--- lib/screens/show_list.dart
+++ lib/screens/show_list.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:linear_progress_bar/linear_progress_bar.dart';
+import 'package:quick_list/app_theme.dart';
 import 'package:quick_list/models/quick_list.dart';
 
 class ShowList extends StatelessWidget {
@@ -10,6 +12,9 @@ class ShowList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int listItemsCount = quickList.listItems?.length ?? 0;
+    int completedListItems = quickList.listItems!.where((item) => item.completed).length;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(quickList.title),
@@ -17,8 +22,14 @@ class ShowList extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(12),
         child: Column(
-        children: [
-            Text('Hello')
+          children: [
+            LinearProgressBar(
+              maxSteps: listItemsCount,
+              currentStep: completedListItems,
+              backgroundColor: progressBarBase,
+              progressColor: progressBarCompleted,
+            ),
+            Text('Hello'),
           ],
         ),
       )
