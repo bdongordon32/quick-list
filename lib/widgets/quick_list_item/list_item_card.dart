fbf4181efcafe624ea9b920ed1783e3005e34c4c
Brandon Gordon <bragor32@gmail.com>
2024-12-30 15:18:43 -0500

    [8] Toggle Completion

--- lib/widgets/quick_list_item/list_item_card.dart
+++ lib/widgets/quick_list_item/list_item_card.dart
@@ -2,18 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:quick_list/models/quick_list_item.dart';
 import 'package:quick_list/widgets/text_input.dart';
 
-class ListItemCard extends StatelessWidget {
-  const ListItemCard(this.listItem, { super.key });
+class ListItemCard extends StatefulWidget {
+  const ListItemCard(this.listItem, {super.key});
 
   final QuickListItem listItem;
+
+  @override
+  State<ListItemCard> createState() => _ListItemCardState();
+}
+
+class _ListItemCardState extends State<ListItemCard> {
+  bool isCompleted = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      isCompleted = widget.listItem.completed;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Checkbox(value: false, onChanged: null),
-        Text(listItem.description)
+        Checkbox(
+          value: isCompleted,
+          onChanged: (bool? isChanged) {
+            if (isChanged!) {
+              setState(() {
+                isCompleted = true;
+              });
+            } else {
+              setState(() {
+                isCompleted = false;
+              });
+            }
+          }
+        ),
+        Text(widget.listItem.description)
       ]
     );
   }
 }
+
+// class ListItemCard extends StatelessWidget {
+  // const ListItemCard(this.listItem, { super.key });
+
+  // final QuickListItem listItem;
+  
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Row(
+  //     children: [
+  //       Checkbox(value: false, onChanged: null),
+  //       Text(listItem.description)
+  //     ]
+  //   );
+  // }
+// }
